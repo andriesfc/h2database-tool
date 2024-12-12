d456f30deb24d55a547fe6e14e05abf60773b767
andriesfc <andries.fc@gmail.com>
2024-12-12 21:01:28 +0200

    Build scan setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,18 @@ pluginManagement {
     includeBuild("build-logic")
 }
 
+plugins {
+    id("com.gradle.enterprise").version("3.7.2")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
 rootProject.name = "h2database-tool"
 
 include(":app")
